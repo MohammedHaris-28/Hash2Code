c92a8dcbc4afee162a441b70b398cdb9a3918520
MohammedHaris-28 <mohammedharis280205@gmail.com>
2026-07-29 18:29:02 +0530

    SEO setup: add sitemap and fix metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import Sitemap from "vite-plugin-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
 
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    Sitemap({
+      hostname: "https://hash2codeteam.vercel.app",
+    }),
+  ],
 
   resolve: {
     alias: {
